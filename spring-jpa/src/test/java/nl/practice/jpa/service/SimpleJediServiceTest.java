f8e285a59965e28fe0799093f01ef14b754827a6
lnardai <laci.nardai@gmail.com>
2018-01-23 09:14:40 +0100

    added Junit 5 featured testcases to SimpleJediServiceTest

--- spring-jpa/src/test/java/nl/practice/jpa/service/SimpleJediServiceTest.java
+++ spring-jpa/src/test/java/nl/practice/jpa/service/SimpleJediServiceTest.java
@@ -1,7 +1,8 @@
 package nl.practice.jpa.service;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Matchers.anyLong;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -11,6 +12,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import nl.practice.jpa.entity.EntityNotFoundException;
 import nl.practice.jpa.entity.Jedi;
 import nl.practice.jpa.repositories.JediRepository;
 
@@ -19,18 +21,32 @@ class SimpleJediServiceTest {
 	private JediRepository repositoryMock = mock(JediRepository.class);
 	private SimpleJediService service;
 	private Jedi exampleJedi = new Jedi("Qui-Gon Jinn");
+	private long quiGonId = 1l;
 
 	@BeforeEach
 	public void configure(){
+		exampleJedi.setId(quiGonId);
 		service = new SimpleJediService(repositoryMock);
 	}
 
 	@Test
 	@DisplayName("get Jedi with Optional value successful")
 	void getExactJedi() {
-
-		when(repositoryMock.findById(anyLong())).thenReturn(Optional.of(exampleJedi));
-		assertEquals("Qui-Gon Jinn", service.getExactJedi(11l).getName());
-
+		when(repositoryMock.findById(quiGonId)).thenReturn(Optional.of(exampleJedi));
+		Jedi jedi = service.getExactJedi(quiGonId);
+		assertAll("multi assert",
+				() -> assertEquals("Qui-Gon Jinn", jedi.getName()),
+				() -> assertEquals(1, jedi.getId())
+		);
 	}
+
+	@Test
+	@DisplayName("get Jedi which is non existent")
+	void getImaginaryJedi() {
+		when(repositoryMock.findById(777l)).thenReturn(Optional.empty());
+		EntityNotFoundException exception = assertThrows(EntityNotFoundException.class,
+				() -> service.getExactJedi(777l).getName());
+		assertEquals("JediId:777", exception.getMessage());
+	}
+
 }
